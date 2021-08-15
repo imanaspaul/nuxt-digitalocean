7dddf6db1dbc695e4a6bfe46cd013fced8603dc8
Manas Paul <manas@codelogicx.com>
2021-08-15 09:21:02 +0530

    :tada: Initial proj setup

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div>
+      <h1 class="text-4xl font-mono font-semibold">
+        <span class="text-green-300">Nuxt</span> &#128157;
+        <span class="text-blue-400">DigitalOcean</span>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
