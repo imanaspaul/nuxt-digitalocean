c1e3d0b35a4aa79dbc7de22187251541a1dcce31
Manas Paul <manas@codelogicx.com>
2021-08-15 10:00:53 +0530

    :sparkles: meta descroiption added

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'nuxt digital ocean boilerplate. Deploy your nuxt js project with Digital Ocean and Docker.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
